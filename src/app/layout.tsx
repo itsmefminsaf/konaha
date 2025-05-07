02bcfbb8a969e494b83854c1b5f2f503db8eabba
itsmefminsaf <itsmefminsaf@gmail.com>
2025-05-07 23:40:34 +0530

    feat: initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Welcome to Hidden Leaf Village",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const RootPage = () => {
+  return (
+    <main>
+      <h1>Welcome to Hidden Leaf Village</h1>
+    </main>
+  );
+};
+
+export default RootPage;
